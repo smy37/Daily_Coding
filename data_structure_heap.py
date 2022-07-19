7716892204d30f371829f6d8269aaa211f1fe00b
ysm0707 <ysm0707@midasit.com>
2022-07-19 14:32:52 +0900

    20220719_heap

--- data_structure_heap.py
+++ data_structure_heap.py
@@ -7,7 +7,7 @@ class Heap():
     def add(self, a:int):
         self.array.append(a)
         cur = len(self.array)-1
-        while self.array[(cur-1)//2]>=self.array[cur] and (cur-1)//2 >=0:
+        while (cur-1)//2 >=0 and self.array[(cur-1)//2]>=self.array[cur]:
             temp = int(self.array[(cur-1)//2])
             self.array[(cur-1)//2] = self.array[cur]
             self.array[cur] = temp
@@ -16,10 +16,14 @@ class Heap():
         return
 
     def delete(self):
-        self.array = self.array[-1] + self.array[1:-1]
+        if len(self.array) == 1:
+            self.array = []
+            return
+        self.array = [self.array[-1]] + self.array[1:-1]
         cur = 0
-        while (self.array[(cur*2+1)] > self.array[cur] or self.array[cur*2+2] > self.array[cur]) and cur < len(self.array):
-            if self.array[(cur)*2+1] >= self.array[cur*2+2]:
+
+        while (cur < len(self.array) and cur*2+2 <len(self.array)) and (self.array[(cur*2+1)] < self.array[cur] or self.array[cur*2+2] < self.array[cur]):
+            if self.array[(cur)*2+1] <= self.array[cur*2+2]:
                 temp = int(self.array[cur*2+1])
                 self.array[cur*2+1] = self.array[cur]
                 self.array[cur] = temp
@@ -29,6 +33,8 @@ class Heap():
                 self.array[cur*2+2] = self.array[cur]
                 self.array[cur] = temp
                 cur = cur*2+2
+        if len(self.array) == 2:
+            self.array = sorted(self.array)
         return
 
 
@@ -37,4 +43,7 @@ test = [5, 8, 9, 10, 4, 3, 11, 2, 4]
 
 for i in test:
     h.add(i)
-print(h.array)
+print('#######')
+for i in range(9):
+    h.delete()
+    print(h.array)
