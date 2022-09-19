00c5fa51824cdf1039ccaa1bd75c5913e3e95ffe
ysm0707 <ysm0707@midasit.com>
2022-09-19 21:30:14 +0900

    20220919_Algorithm

--- bfs_trpRoute.py
+++ bfs_trpRoute.py
@@ -1,12 +1,16 @@
-tickets = [["ICN", "AAA"], ["ICN", "AAA"], ["ICN", "AAA"], ["AAA", "ICN"], ["AAA", "ICN"]]
+tickets = [["ICN", "COO"], ["ICN", "BOO"], ["COO", "ICN"]]
 t1 =[["ICN", "JFK"], ["HND", "IAD"], ["JFK", "HND"]]
 t2 = [["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]
+
+
+
 r_final = []
-def dfs(stack, visited, graph, tickets):
-    if len(visited) == len(tickets):
-        if [visited[-1], stack[-1]] in tickets:
+def dfs(stack, visited, graph, tickets, cri):
+    if len(visited) == cri:
+        if tickets==[]:
+            visited.append(stack[-1])
             r_final.append(visited)
-            return
+            return r_final
         else:
             return False
     else:
@@ -15,12 +19,13 @@ def dfs(stack, visited, graph, tickets):
             return False
         for i in graph[temp]:
             if [temp, i] in tickets:
-                dfs(stack+[i], visited+[temp], graph, tickets)
-                print(stack, visited)
+                idx = tickets.index([temp, i])
+                dfs(stack+[i], visited+[temp], graph, tickets[:idx]+tickets[idx+1:], cri)
+
 
 
 def solutions(tickets):
-    final = []
+    global r_final
     graph = {}
     for i in tickets:
         if i[0] not in graph:
@@ -28,15 +33,19 @@ def solutions(tickets):
             if i[1] not in graph:
                 graph[i[1]] = []
         else:
+            if i[1] not in graph:
+                graph[i[1]] = []
             graph[i[0]].append(i[1])
     for i in graph:
         graph[i] = sorted(graph[i])
-
+    print(graph)
+    cri = len(tickets)
     stack = []
     visited = []
     stack.append("ICN")
-    final.append(dfs(stack,visited,graph,tickets))
-    print(r_final)
-    return r_final
-solutions(t2)
+    dfs(stack,visited,graph,tickets, cri)
+    r_final = sorted(r_final)
+    print(r_final[0])
+    return r_final[0]
+solutions(tickets)
 
