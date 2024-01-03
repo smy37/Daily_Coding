305b1df8e67d6729cf4286cfe5353805e571d180
ysm0707 <ysm0707@midasit.com>
2024-01-03 17:58:50 +0900

    20240103_Algorithm

--- Backjoon/Gold3_CastleDefence.py
+++ Backjoon/Gold3_CastleDefence.py
@@ -13,20 +13,22 @@ dx = [0, -1, 0]
 dy = [-1, 0, 1]
 def update_board(b : list):
     return b[:len(b)-2]+[[0 for _ in range(len(b[0]))]]
-def dfs(s, visited,b, cur_d, D):
+def dfs(s, visited,b, D):
+    global del_l
     while s:
         t = s.popleft()
         if b[t[0]][t[1]] ==1 :
-            b[t[0]][t[1]] = 0
+            del_l.append([t[0],t[1]])
+            # b[t[0]][t[1]] = 0
             return True
         else:
             for i in range(3):
                 nx = t[0] + dx[i]
                 ny = t[1] + dy[i]
+                cur_d = t[2]
                 if 0<=nx<len(b) and 0<=ny<len(b[0]) and (nx,ny) not in visited and cur_d+1 <= D:
-                    s.append([nx,ny])
+                    s.append([nx,ny, cur_d+1])
                     visited[(nx,ny)] = 1
-            cur_d+=1
     return False
 
 permute = permutations(range(M), 3)
@@ -36,19 +38,24 @@ for i in permute:
     t_board = copy.deepcopy(board)
     cnt = 0
     for j in range(N):
+        del_l = []
         for k in i:
             dq = deque()
-            dq.append([len(t_board)-1,k])
+            dq.append([len(t_board)-1,k, 0])
             visited = {}
             visited[(len(t_board)-1, k)] = 1
 
-            if dfs(dq, visited, t_board, 0, D):
-                cnt +=1
-            # print(k, cnt)
-            # for x in board:
-            #     print(x)
-            # print()
+            dfs(dq, visited, t_board, D)
+
+        for x in del_l:
+            t_board[x[0]][x[1]] = 0
+        s = {}
+        for x in del_l:
+            s[tuple(x)] = 1
+        cnt += len(s.keys())
         t_board = update_board(t_board)
+
     g_cnt = max(g_cnt, cnt)
+
 
 print(g_cnt)
