2c471fe0b66266a4207a1fc1a4ae8d58f62bd3ad
smy37 <seongminyang37@naver.com>
2025-03-31 10:20:34 +0900

    add_comment

--- al_20250331.py
+++ al_20250331.py
@@ -7,6 +7,7 @@ board =[]
 for _ in range(N):
     board.append(list(map(int, sys.stdin.readline().split())))
 
+## 절대값을 풀어준 총 4가지의 경우의 수에 대해 처리, sorting 되면 (a,b)와 (c,d)의 구분은 무의미 해지므로 경우의수가 4개에서 2개로 준다.
 plus_board = sorted(copy.deepcopy(board), key = lambda x : x[0]+x[1])
 minus_board = sorted(copy.deepcopy(board), key = lambda x : x[0]-x[1])
 
