b15214d50a118ded8d61d2480be90bcedd552023
smy37 <seongminyang37@naver.com>
2025-04-03 21:14:01 +0900

    union_find add for lineGroups

--- al_20250402.py
+++ al_20250402.py
@@ -15,7 +15,9 @@ def intersect(p, p2, q, q2):
 
     if denom == 0:
         if cross(qp, r) == 0:
-            return True
+            if (max(p[0], p2[0]) >= min(q[0], q2[0]) and max(p[1], p2[1]) >= min(q[1], q2[1]))\
+                    and (max(q[0], q2[0]) >= min(p[0], p2[0]) and max(q[1], q2[1]) >= min(p[1], p2[1])):
+                return True
         return False
 
     t = cross(qp, s)/ denom
@@ -23,8 +25,19 @@ def intersect(p, p2, q, q2):
 
     return 0<=t<=1 and 0<=u<=1
 
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
 
+def union(x, y):
+    x_parent = find(x)
+    y_parent = find(y)
 
+    if x_parent <= y_parent:
+        parent[y_parent] = x_parent
+    else:
+        parent[x_parent] = y_parent
 N = int(sys.stdin.readline())
 
 node_l = []
@@ -32,26 +45,21 @@ for _ in range(N):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
     node_l.append([[x1,y1], [x2,y2]])
 
+parent = [i for i in range(N)]
+
 line_group = {i:-1 for i in range(N)}
 cur = 1
 for i in range(N):
-    for j in range(N):
-        if i == j:
-            continue
+    for j in range(i+1, N):
         if intersect(node_l[i][0], node_l[i][1], node_l[j][0], node_l[j][1]):
-            if line_group[i] == -1 and line_group[j] == -1:
-                line_group[i] = cur
-                line_group[j] = cur
-                cur += 1
-            elif line_group[i] == -1:
-                line_group[i] = line_group[j]
-            elif line_group[j] == -1:
-                line_group[j] = line_group[i]
+            union(i, j)
+
 answer = {}
-for k in line_group:
-    if line_group[k] not in answer:
-        answer[line_group[k]] = 0
-    answer[line_group[k]] += 1
+for i in parent:
+    root = find(i)
+    if root not in answer:
+        answer[root] = 0
+    answer[root] += 1
 
 print(len(answer))
 print(max(answer.values()))
