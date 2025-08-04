89f95d277576a7503e15e8702b51c8462fbb6f07
smy37 <seongminyang37@naver.com>
2025-08-05 00:09:40 +0900

    20250805_two pointer for crossing !리뷰& - #32358

--- classify_by_day/al_20250804.py
+++ classify_by_day/al_20250804.py
@@ -26,14 +26,17 @@ N = int(sys.stdin.readline())
 
 
 ### 2. Second Approach
-right_tree = []
-left_tree = []
 answer = 0
 cur_pos = 0
+
+right_tree = []
+left_tree = []
+
+rt_idx = 0
+lt_idx = 0
 for _ in range(N):
     cmd = list(map(int, sys.stdin.readline().split()))
-    rt_idx = 0
-    lt_idx = 0
+
     if cmd[0] == 1:
         if cur_pos <= cmd[1]:
             right_tree.append(cmd[1])
@@ -42,7 +45,7 @@ for _ in range(N):
 
     elif cmd[0] == 2:
         right_tree.sort()
-        left_tree.sort()
+        left_tree.sort(reverse= True)
 
         while 1:
             if rt_idx < len(right_tree) and lt_idx < len(left_tree):
@@ -67,5 +70,17 @@ for _ in range(N):
                 break
         right_tree = []
         left_tree = []
+        rt_idx = 0
+        lt_idx = 0
 
 print(answer)
+
+
+explain = """
+실시간으로 업데이트 되는 상황에서 최소 거리가 되는 점을 찾아야 하므로 처음에 heap으로 푸는 것을 시도하였다. 
+그러나 heap에 들어간 값이 cur_pos가 바뀜에 따라 의미가 없어지므로 heap을 사용하는 건 적절하지 않았다.
+cur_pos를 기준으로 왼쪽에 있는 배열을 만들고 오른쪽에 있는 배열을 만든다음에 cmd 2가 들어오면 cur_pos를 
+중점으로 왼쪽 또는 오른쪽으로 왔다갔다 하면서 청소가 진행된다. 이때, 왼쪽에 대한 인덱스와 오른쪽에 대한 인덱스를
+증가시켜 가면서 이동거리를 측정하면 된다. 하나 유의해야 할점은 left_tree 배열은 정렬시 거꾸로 해야 한다. 
+cur_pos를 기준으로 더 가까운 거리가 배열의 첫번째 부터 나오게 하려면.
+"""
