8ff6da6f1386310340a1b888b150d410a3b87517
smy37 <69130114+smy37@users.noreply.github.com>
2025-09-13 23:50:14 +0900

    20250911_다이나믹 프로그래밍 update time !리뷰& -#13156

--- classify_by_day/al_20250911.py
+++ classify_by_day/al_20250911.py
@@ -14,18 +14,23 @@ c, m = map(int, sys.stdin.readline().split())
 # print(max(dp))
 
 ### 2. Second Approach
-dp = [0]*(c+1)
-cur_m = list(map(int, sys.stdin.readline().split()))
-for i in range(c):
-    dp[i + 1] = max(dp[i + 1], cur_m[i])
-for _ in range(m-1):
-    cur_m = list(map(int, sys.stdin.readline().split()))
+dp = [-float("inf")]*(c+1)
+dp[0] = 0
+
+for _ in range(m):
     new_dp = copy.deepcopy(dp)
-    for i in range(c):
-        dp[i+1] = max(dp[i+1], cur_m[i])
+    cur_m = list(map(int, sys.stdin.readline().split()))
+
     for i in range(c):
         for j in range(c-(i)):
-            if dp[j] > 0:
-                dp[j+i+1] = max(dp[j+i+1], cur_m[i]+dp[j])
-    print(dp)
-print(dp)
+            if dp[j] != -float("inf"):
+                new_dp[j+i+1] = max(new_dp[j+i+1], cur_m[i]+dp[j])
+    dp = new_dp
+
+print(max(dp))
+
+explain = """
+0번째 항을 0으로 초기화 해주고 dp[j] != -float("inf") 조건으로 하는게 중요하다.
+특정 상인에게서 구매를 시작하는 조건을 위해서는 dp[0] = 0가 필요하다. 또한 != -float("inf") 조건이 없으면
+구매 상태가 없는데도 이어서 구매를 하게 되므로 추가해줘야 한다. 
+"""
