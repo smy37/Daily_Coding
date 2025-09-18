def7a34a1fee7b6bf99bd919acdd07f046a9d003
smy37 <69130114+smy37@users.noreply.github.com>
2025-09-18 21:51:26 +0900

    20250918_fast prime finding !리뷰& -#

--- classify_by_day/al_20250916.py
+++ classify_by_day/al_20250916.py
@@ -9,22 +9,29 @@ while 1:
     num_list.append(n)
 
 upper_limit = max(num_list)
-dp = [0]*(upper_limit+1)
+is_prime = [False, False] + [True]*(upper_limit-1)
 
-for i in range(1, upper_limit//2+1):
-    dp[i*2] = 1
+for i in range(4, upper_limit+1, 2):
+    is_prime[i] = False
 
-prime = {}
-for i in range(3, upper_limit+1):
-    if dp[i] == 0:
-        prime[i] = 1
-        for j in range(1, upper_limit//i + 1):
-            dp[i*j] = 1
+i = 3
+while i*i <= upper_limit:
+    if is_prime[i]:
+        for j in range(i*i, upper_limit+1, i*2):
+            is_prime[j] = False
+    i += 2
 
-prime_v = list(prime.keys())
+primes = [p for p in range(3, upper_limit//2, 2) if is_prime[p]]
+
+out = []
 for n in num_list:
-    for i in range(len(prime_v)-1, 0, -1):
-        p = prime_v[i]
-        if n-p in prime:
-            print(f"{n} = {n-p} + {p}")
+    found = False
+    for p in primes:
+        if is_prime[p] and is_prime[n-p]:
+            print(f"{n} = {p} + {n-p}")
             break
+
+explain = """
+소수를 구하는 알고리즘인 에라스토네체를 최대한 속도 측면에서 빠르게 하는 것이 중요했던 문제.
+범위 내 소수만 구한다면 그 외 나머지는 브루트 포스를 통해 찾을 수 있다.
+"""
