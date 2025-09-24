bbc0641aed929571ed2c5eaa4cde6abbc6732a40
smy37 <69130114+smy37@users.noreply.github.com>
2025-09-25 00:34:13 +0900

    20250925_rectangle cover !리뷰& -#2527

--- classify_by_day/al_20250922.py
+++ classify_by_day/al_20250922.py
@@ -2,8 +2,21 @@ import sys
 
 for _ in range(4):
     temp_line = list(map(int, sys.stdin.readline().split()))
-    x1,y1,p1,q1 = temp_line[:4]
-    x2,y2,p2,q2 = temp_line[4:]
+    l_x1,l_y1,r_x1,r_y1 = temp_line[:4]
+    l_x2,l_y2,r_x2,r_y2 = temp_line[4:]
 
-    if (p1<x2 or p2 < x1) and (q1<y2 or q2 < y1):
+    if (l_x1 > r_x2 or l_x2 > r_x1 or l_y1 > r_y2 or l_y2 > r_y1):
         print("d")
+    elif (l_x1 == r_x2 and (l_y1 == r_y2 or r_y1 == l_y2)) or\
+    (r_x1 == l_x2) and (r_y1 == l_y2 or l_y1 == r_y2):
+        print("c")
+    elif (l_x2 < r_x1 and l_x1 < r_x2) and ((l_y2 < r_y1) and (l_y1 < r_y2)):
+        print("a")
+    else:
+        print("b")
+    
+
+explain = """
+사각형 두개중에 한개를 고정하고 나머지 한개를 움직이면서 생각하면 더 쉬운 문제.
+안만나는 경우, 점에서 만나는 경우, 겹치는 부분이 직사각형인 부분, 겹치는 부분이 선분인 선으로 생각해준다.
+"""
