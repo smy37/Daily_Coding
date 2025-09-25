6621a67ae55f2cf6a9729237b4ad84acdf1d9ab0
smy37 <69130114+smy37@users.noreply.github.com>
2025-09-26 00:07:10 +0900

    20250925_min value !리뷰& -#1049

--- classify_by_day/al_20250923.py
+++ classify_by_day/al_20250923.py
@@ -1,12 +1,21 @@
 import sys
+import math
 
 N, M = map(int , sys.stdin.readline().split())
 
-price = []
+min_six_p, min_one_p = float("inf"), float("inf")
 for _ in range(M):
     six_p, one_p = map(int, sys.stdin.readline().split())
 
     if 6*one_p < six_p:
-        price.append([6*one_p, one_p])
+        min_six_p = min(min_six_p, 6*one_p)
     else:
-        price.append([six_p, one_p])
+        min_six_p = min(min_six_p, six_p)
+    min_one_p = min(min_one_p, one_p)
+
+print(min(math.ceil(N/6)*min_six_p, N//6*min_six_p+N%6*min_one_p))
+
+explain = """
+목록중에서 6개를 살때 최소와 한개를 살때 최소값을 기록해둔다. 이때, 1개짜리를 6개 샀을 때, 최소가 될 수 있으니
+이도 체크해서 최소값을 구한다.
+"""
