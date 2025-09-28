97730d48a5227127e9ace2959312a4eaca22220c
smy37 <69130114+smy37@users.noreply.github.com>
2025-09-28 23:10:04 +0900

    20250928_원으로의 이동과 거리 !리뷰& -#32282

--- classify_by_day/al_20250925.py
+++ classify_by_day/al_20250925.py
@@ -4,7 +4,17 @@ import math
 X, Y, c = map(int, sys.stdin.readline().split())
 dist = math.sqrt(X**2+Y**2)
 
-if dist%c == 0:
-    print(dist//c)
+if X==0 and Y == 0:
+    print(0)
+elif dist < c:
+    print(2)
 else:
-    print(int(dist/c)+1)
+    if dist%c == 0:
+        print(int(dist//c))
+    else:
+        print(int(dist/c)+1)
+
+explain = """
+도달해야 하는 길이와 반지름을 비교하면 된다. 다만, 도달해야 하는 길이가 0일 때와
+도달해야 하는 길이가 반지름보다 짧은 경우를 예외 처리 해줘야 한다.
+"""
