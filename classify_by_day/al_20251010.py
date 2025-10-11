7ed9d0757353b12f6560231bc55dcb99e1878deb
smy37 <69130114+smy37@users.noreply.github.com>
2025-10-12 01:02:37 +0900

    20251010_tree with dfs&dp !리뷰& -#25691

--- classify_by_day/al_20251010.py
+++ classify_by_day/al_20251010.py
@@ -16,12 +16,12 @@ dp = {i: [0 for _ in range(k+1)] for i in range(n)}
 
 apple_cnt = list(map(int, sys.stdin.readline().split()))
 
-stack = [[0, 1, apple_cnt[0]]]
+stack = [[0, 1]]
 visited = {0:True}
 dp[0][1] = apple_cnt[0]
 
 def dfs(s: list):
-    cur_n, node_cnt, apple_n = s.pop()
+    cur_n, node_cnt = s.pop()
 
     if node_cnt >= k:
         return
@@ -30,11 +30,25 @@ def dfs(s: list):
         if next_n not in visited:
             visited[next_n] = True
             next_apple = apple_cnt[next_n]
-            dp[next_n][node_cnt+1] = max(dp[next_n][node_cnt+1], apple_n+next_apple)
-            dfs(s+[[next_n, node_cnt+1, apple_n+next_apple]])
-            for idx, a_c in enumerate(dp[next_apple]):
+            for idx in range(1, k+1):
+                a_c = dp[cur_n][idx]
+                if idx>=k:
+                    break
+                dp[next_n][idx + 1] = max(dp[next_n][idx + 1], a_c+next_apple)
+            dfs(s+[[next_n, node_cnt+1]])
+            for idx in range(1, k+1):
+                a_c = dp[next_n][idx]
                 dp[cur_n][idx] = max(dp[cur_n][idx], a_c)
+
+
 dfs(stack)
-print(dp)
+print(max(dp[0]))
+
+explain = """
+트리에서 특정 노드에 도달 했을 때, 현재 노드를 도달하는데 가능한 소요 노드 개수에 대해서 그 값의 
+1을 더한 것으로 다음 노드의 dp를 업데이트 해준다. 그리고 dfs를 한번더 호출하고 그 호출이 끝난다음에 현재
+노드의 dp를 다음 노드의 dp를 기반으로 업데이트 해준다. 
+단, 이때 첫번째 노드는 무조건 방문해야 하고 그래서 소요 노드가 0인것은 불가함을 주의해줘야 한다. 
+"""
 
 
