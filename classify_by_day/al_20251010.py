f0696b0dca4cfc92f38a41e5d21d5e1705da94b1
smy37 <69130114+smy37@users.noreply.github.com>
2025-10-11 23:57:25 +0900

    트리 순회 초안

--- classify_by_day/al_20251010.py
+++ classify_by_day/al_20251010.py
@@ -1,33 +1,40 @@
 import sys
 
-N = int(sys.stdin.readline())
+n, k = map(int, sys.stdin.readline().split())
 
-graph = {}
+tree = {}
+for _ in range(n-1):
+    p, c = map(int, sys.stdin.readline().split())
+    if p not in tree:
+        tree[p] = {}
+    if c not in tree:
+        tree[c] = {}
+    tree[p][c] = True
+    tree[c][p] = True
 
-for i in range(N):
-    graph[i+1] = int(sys.stdin.readline())
+dp = {i: [0 for _ in range(k+1)] for i in range(n)}
 
-### Method 1. 표에서 위의 값들을 담는 딕셔너리 한개와 아래 값들을 담는 스택 한개 사용 (정답 코드임)
-# answer = {}
-#
-# for i in range(N):
-#     if i + 1 not in answer:
-#         temp = {i+1 : False}
-#         s = [graph[i+1]]
-#
-#         while s:
-#             t = s.pop()
-#             temp[t] = True
-#             if t not in temp:
-#                 s.append(graph[t])
-#         for t in temp:
-#             if temp[t] == False:
-#                 break
-#         else:
-#             for t in temp:
-#                 answer[t] = True
-#
-# print(len(answer))
-# answer_l = sorted(answer.keys())
-# for i in answer_l:
-#     print(i)
+apple_cnt = list(map(int, sys.stdin.readline().split()))
+
+stack = [[0, 1, apple_cnt[0]]]
+visited = {0:True}
+dp[0][1] = apple_cnt[0]
+
+def dfs(s: list):
+    cur_n, node_cnt, apple_n = s.pop()
+
+    if node_cnt >= k:
+        return
+
+    for next_n in tree[cur_n]:
+        if next_n not in visited:
+            visited[next_n] = True
+            next_apple = apple_cnt[next_n]
+            dp[next_n][node_cnt+1] = max(dp[next_n][node_cnt+1], apple_n+next_apple)
+            dfs(s+[[next_n, node_cnt+1, apple_n+next_apple]])
+            for idx, a_c in enumerate(dp[next_apple]):
+                dp[cur_n][idx] = max(dp[cur_n][idx], a_c)
+dfs(stack)
+print(dp)
+
+
