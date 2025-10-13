dc1d47570e7a843e914df7ee8fb7681b09b7a0fc
smy37 <69130114+smy37@users.noreply.github.com>
2025-10-14 00:54:29 +0900

    20251013_dp with 3중 for문 !리뷰& -#14550

--- classify_by_day/al_20251013.py
+++ classify_by_day/al_20251013.py
@@ -1,10 +1,22 @@
 import sys 
 
 def dp(num_l: list, N, S, T):
-    memory = [[-float("inf") for _ in range(T)] for _ in range(N+2)]
+    answer = -float("inf")
+    memory = [[-float("inf") for _ in range(N+2)] for _ in range(T)]
     for i in range(1,S+1):
-        memory[i][0] = max(memory[i][0], num_l[i-1])
+        memory[0][i] = max(memory[0][i], num_l[i-1])
+    for i in range(1, T):
+        for j in range(N+2):
+            if memory[i-1][j] != -float("inf"):
+                for s in range(1, S+1):
+                    if j+s >= N+1:
+                        memory[i][N+1] = max(memory[i][N+1], memory[i-1][j])
+                    else:
+                        memory[i][j+s] =\
+                            max(memory[i][j+s], memory[i-1][j]+num_l[j+s-1])
+        answer = max(answer, memory[i][N+1])
 
+    return answer
 
 N, S, T = map(int, sys.stdin.readline().split())
 total_num = []
@@ -15,8 +27,14 @@ while 1:
         break
     else:
         if len(total_num) >= N:
-
+            print(dp(total_num, N, S, T))
             N, S, T = cur_l
             total_num = []
         else:
             total_num.extend(cur_l)
+print(dp(total_num, N, S, T))
+
+explain = """
+T개의 단계동안 주사위를 던질 때, 각 단계별로 발판과 최대값을 저장해 둔다.
+T동안 이전 단계에서의 발판에서 1~S만큼 움직여서 현재 발판을 업데이트 해준다.
+"""
