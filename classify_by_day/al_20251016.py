1d4e844b64b2249c346a71547e16979f9bae4efd
smy37 <69130114+smy37@users.noreply.github.com>
2025-10-17 00:16:10 +0900

    20251016_hash for geometry !리뷰& -#2121

--- classify_by_day/al_20251016.py
+++ classify_by_day/al_20251016.py
@@ -1,7 +1,7 @@
 import sys 
 
 N = int(sys.stdin.readline())
-x_dist, y_dist = map(int, sys.stdin.readline())
+x_dist, y_dist = map(int, sys.stdin.readline().split())
 cord_l = {}
 
 for _ in range(N):
@@ -10,6 +10,22 @@ for _ in range(N):
         cord_l[x] = {}
     cord_l[x][y] = 1
 
-x_list = list(cord_l.keys())
+x_list = sorted(list(cord_l.keys()))
+
+answer = 0
+for x1 in x_list:
+    x2 = x1+x_dist
+    if x2 in cord_l:
+        for y1 in cord_l[x1]:
+            y2 = y1+y_dist
+            if y2 in cord_l[x1] and y1 in cord_l[x2] and y2 in cord_l[x2]:
+                answer += 1
+
+print(answer)
+
+explain = """
+좌표를 해시에 저장해두고 현재 좌표를 기준으로 직사각형을 이루는 3점이 존재하는지 판단해준다.
+"""
+
 
 
