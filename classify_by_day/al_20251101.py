2bc38f0a09040f23164321c4097c9eb5f9235e29
smy37 <69130114+smy37@users.noreply.github.com>
2025-11-02 00:35:04 +0900

    20251101_직사각형의 교차 !리뷰& -#15973

--- classify_by_day/al_20251101.py
+++ classify_by_day/al_20251101.py
@@ -6,24 +6,28 @@ x3, y3, x4, y4 = map(int, sys.stdin.readline().split())
 if x3 > x2 or x4 < x1 or y3 > y2 or y4 < y1:
     print("NULL")
 elif x3 == x2:
-    if y3 == y2:
+    if y3 == y2 or y4 == y1:
         print("POINT")
     else:
         print("LINE")
 elif x4 == x1:
-    if y4 == y1:
+    if y4 == y1 or y3 == y2:
         print("POINT")
     else:
         print("LINE")
 elif y3 == y2:
-    if x3 == x2:
+    if x3 == x2 or x4 == x1:
         print("POINT")
     else:
         print("LINE")
 elif y4 == y1:
-    if x4 == x1:
+    if x4 == x1 or x3 == x2:
         print("POINT")
     else:
         print("LINE")
 else:
     print("FACE")
+
+explain = """
+사각형 하나를 고정해두고 다른 사각형을 움직이며 조건을 수립하는 것이 핵심.
+"""
