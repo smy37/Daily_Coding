ca948a8f8cce080dc29f1b09c1a789c941100aa6
smy37 <69130114+smy37@users.noreply.github.com>
2025-11-05 00:56:45 +0900

    20251104_완전탐색 with 조합 !리뷰& -#15661

--- classify_by_day/al_20251104.py
+++ classify_by_day/al_20251104.py
@@ -7,6 +7,10 @@ board = []
 for _ in range(N):
     row = list(map(int, sys.stdin.readline().split()))
     board.append(row)
+pair= {}
+for i in range(N):
+    for j in range(i+1, N):
+        pair[(i, j)] = board[i][j] + board[j][i]
 
 upper_limit = N//2
 
@@ -19,6 +23,21 @@ for n in range(1, upper_limit+1):
         for i in range(len(comb)):
             for j in range(i+1, len(comb)):
                 n1, n2 = comb[i], comb[j]
-                t1 += (board[n1][n2]+board[n2][n1])
+                t1 += pair[(n1, n2)]
 
+        rest = [i for i in range(N) if i not in set(comb)]
+
+        for i in range(len(rest)):
+            for j in range(i+1, len(rest)):
+                n1, n2 = rest[i], rest[j]
+                t2 += pair[(n1, n2)]
         
+        answer = min(answer, abs(t1-t2))
+
+
+print(answer)
+
+explain = """
+팀을 나누는 것을 combination을 통해서 수행하였다. 단, 1팀은 0 2팀은 1로 표지해서
+DFS를 통해 팀의 경우의 수를 조회하는 방법도 존재한다.
+"""
