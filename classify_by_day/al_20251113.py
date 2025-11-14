6a93c04b39513df5c967c9156cb355307dc5b02f
smy37 <69130114+smy37@users.noreply.github.com>
2025-11-14 23:58:49 +0900

    20251113

--- classify_by_day/al_20251113.py
+++ classify_by_day/al_20251113.py
@@ -1,26 +1,54 @@
 import sys
+import math 
 
 N = int(sys.stdin.readline())
 
-num_l = []
+### 1. Fisrt Approach
+# num_l = []
+# num_dict = {}
+# for _ in range(N):
+#     num = int(sys.stdin.readline())
+#     num_l.append(num)
+
+
+# next_num_l = sorted(num_l, reverse=True)
+
+# for i in range(N):
+#     if next_num_l[i] not in num_dict:
+#         num_dict[next_num_l[i]] = 0
+#         for j in range(i+1, N):
+#             if next_num_l[i]%next_num_l[j] == 0:
+#                 num_dict[next_num_l[i]] += 1
+
+
+# for n in num_l:
+#     print(num_dict[n])
+
+
+### 2. Second Approach
 num_dict = {}
-for _ in range(N):
+num_l = []
+for i in range(N):
     num = int(sys.stdin.readline())
     num_l.append(num)
 
+    if num not in num_dict:
+        num_dict[num] = 0
+    num_dict[num] += 1
 
-next_num_l = sorted(num_l, reverse=True)
-
-for i in range(N):
-    if next_num_l[i] not in num_dict:
-        num_dict[next_num_l[i]] = 0
-        for j in range(i+1, N):
-            if next_num_l[i]%next_num_l[j] == 0:
-                num_dict[next_num_l[i]] += 1
-
+checked = {}
 
 for n in num_l:
-    print(num_dict[n])
-
-
+    if n in checked:
+        print(checked[n])
+    else:
+        temp = 0
+        for i in range(1, int(math.sqrt(n))+1):
+            if n % i == 0 and i in num_dict:
+                rest = n//i
+                temp += num_dict[i]
+                if i != rest and rest in num_dict:
+                    temp += num_dict[rest]
+        checked[n] = temp-1
+        print(checked[n])
 
