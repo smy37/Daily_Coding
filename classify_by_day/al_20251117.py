bddc5a60dec13c4cb1f127d1a361f0d6173452e4
smy37 <seongminyang37@gmail.com>
2025-11-18 00:11:35 +0900

    20251117_union&find !리뷰& -#7511

--- classify_by_day/al_20251117.py
+++ classify_by_day/al_20251117.py
@@ -1,1 +1,44 @@
+import sys
 
+def union(a, b, parent_dict):
+    parent_a = find(a, parent_dict)
+    parent_b = find(b, parent_dict)
+
+    if a <= b:
+        parent_dict[parent_b] = parent_a
+    else:
+        parent_dict[parent_a] = parent_b
+
+def find(cur: int, parent_dict: dict):
+    parent = parent_dict[cur]
+    if cur != parent:
+        parent = find(parent, parent_dict)
+    parent_dict[cur] = parent
+    return parent
+
+T = int(sys.stdin.readline())
+
+for number in range(T):
+    user_num = int(sys.stdin.readline())
+    relation_num = int(sys.stdin.readline())
+    parent_d = {i: i for i in range(user_num)}
+    for i in range(relation_num):
+        s, t = map(int, sys.stdin.readline().split())
+        union(s, t, parent_d)
+
+    print(f"Scenario {number+1}:")
+    q_num = int(sys.stdin.readline())
+    for _ in range(q_num):
+        a, b = map(int, sys.stdin.readline().split())
+        p_a, p_b = find(a, parent_d), find(b, parent_d)
+
+        if p_a == p_b:
+            print(1)
+        else:
+            print(0)
+    print()
+
+explain ="""
+disjoint 집합을 이용해서, 즉 union find를 이용해서 답을 구할 수 있다. 
+항상 find에서 경로 압축을 해줘야 하는 것을 잊지 말자.
+"""
