feaa93bdf9228cbc1d1deaa8859ac4699d42602e
smy37 <seongminyang37@gmail.com>
2025-11-18 23:13:02 +0900

    20251118 초안

---
+++ classify_by_day/al_20251118.py
@@ -0,0 +1,20 @@
+import sys
+
+answer = 0
+
+N, M = map(int, sys.stdin.readline().split())
+spend_time = list(map(int, sys.stdin.readline().split()))
+memory = [0]*M
+
+while spend_time:
+    cur = min(memory)
+
+    for i in range(M):
+        memory[i] -= cur
+    answer += cur
+    for i in range(M):
+        if memory[i] == 0:
+            t = spend_time.pop()
+            memory[i] = t
+
+print(answer+max(memory))
