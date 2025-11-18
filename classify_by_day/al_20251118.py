81559a6d23923205085b75180da7680a057a3014
smy37 <seongminyang37@gmail.com>
2025-11-19 00:15:37 +0900

    20251118_그리디 스택

--- classify_by_day/al_20251118.py
+++ classify_by_day/al_20251118.py
@@ -4,6 +4,7 @@ answer = 0
 
 N, M = map(int, sys.stdin.readline().split())
 spend_time = list(map(int, sys.stdin.readline().split()))
+spend_time.sort()
 memory = [0]*M
 
 while spend_time:
@@ -13,8 +14,12 @@ while spend_time:
         memory[i] -= cur
     answer += cur
     for i in range(M):
-        if memory[i] == 0:
+        if memory[i] == 0 and len(spend_time) > 0:
             t = spend_time.pop()
             memory[i] = t
 
 print(answer+max(memory))
+
+explain = """
+시간이 가장 긴 것 부터 작업을 수행하면 정답을 구할 수 있다. 시간의 리스트는 스택에 넣고 정렬해둔다.
+"""
