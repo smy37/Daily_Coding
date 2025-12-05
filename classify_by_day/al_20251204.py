4b1056587d28efe704f637f4cf565fd5c85b4191
smy37 <seongminyang37@gmail.com>
2025-12-05 21:27:47 +0900

    20251205_greedy&max_value !리뷰& -#55

--- classify_by_day/al_20251204.py
+++ classify_by_day/al_20251204.py
@@ -1,27 +1,43 @@
 from typing import List
 
 class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        cur_idx = 0
-        visit = {cur_idx: True}
-        while 0 <= cur_idx < len(nums)-1:
-            visit[cur_idx] = True
-            next_idx = cur_idx + nums[cur_idx]
-            if next_idx <= cur_idx:
-                while 1:
 
-                    next_idx -= 1
-                    if next_idx not in visit:
-                        visit[next_idx] = True
-                        break
-            cur_idx = next_idx
-        if cur_idx >= len(nums)-1:
+    ## 1. First Approach
+    # def canJump(self, nums: List[int]) -> bool:
+    #     cur_idx = 0
+    #     visit = {cur_idx: True}
+    #     while 0 <= cur_idx < len(nums)-1:
+    #         visit[cur_idx] = True
+    #         next_idx = cur_idx + nums[cur_idx]
+    #         if next_idx <= cur_idx:
+    #             while 1:
+    #
+    #                 next_idx -= 1
+    #                 if next_idx not in visit:
+    #                     visit[next_idx] = True
+    #                     break
+    #         cur_idx = next_idx
+    #     if cur_idx >= len(nums)-1:
+    #         return True
+    #     else:
+    #         return False
+    ## 2. Second Approach
+    def canJump(self, nums: List[int]) -> bool:
+        cur_reach = 0
+        for i in range(len(nums)):
+            if i > cur_reach:
+                return False
+            cur_reach = max(cur_reach, nums[i]+i)
+        if cur_reach >= len(nums)-1:
             return True
         else:
             return False
-
 
 if __name__ == "__main__":
     test_case = [3,2,1,0,4]
     sol = Solution()
     print(sol.canJump(test_case))
+
+    explain = """
+    Update the maximum reachable index and compare it with the current index to check if the position is reachable.
+    """
