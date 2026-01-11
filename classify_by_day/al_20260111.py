b43785ebf2c2c0b6a3afac7869f5db84138e33e2
smy37 <seongminyang37@gmail.com>
2026-01-11 17:20:01 +0900

    20260111_binary search optimization

--- classify_by_day/al_20260111.py
+++ classify_by_day/al_20260111.py
@@ -3,19 +3,44 @@ from bisect import bisect_left
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        min_v = min(nums)
-        idx_min = nums.index(min_v)
-        rotated_dist = len(nums) - idx_min
-        sorted_nums = nums[idx_min:] + nums[:idx_min]
-        target_idx = bisect_left(sorted_nums, target)
-        if target_idx >= len(nums) or sorted_nums[target_idx] != target:
-            return -1
-        rotated_target_idx = target_idx - rotated_dist
+        # min_v = min(nums)
+        # idx_min = nums.index(min_v)
+        # rotated_dist = len(nums) - idx_min
+        # sorted_nums = nums[idx_min:] + nums[:idx_min]
+        # target_idx = bisect_left(sorted_nums, target)
+        # if target_idx >= len(nums) or sorted_nums[target_idx] != target:
+        #     return -1
+        # rotated_target_idx = target_idx - rotated_dist
+        #
+        # if rotated_target_idx < 0:
+        #     return rotated_target_idx + len(nums)
+        # else:
+        #     return rotated_target_idx
 
-        if rotated_target_idx < 0:
-            return rotated_target_idx + len(nums)
-        else:
-            return rotated_target_idx
+        left, right = 0, len(nums)-1
+
+        while left <= right:
+            mid = (left+right)//2
+
+            if nums[mid] == target:
+                return mid
+
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid-1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid -1
+
+        return -1
+
+
+
+
 
 
 if __name__ == "__main__":
