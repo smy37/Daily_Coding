b2f134e8a57337b79151c31a78d88dbc2ee41562
smy37 <seongminyang37@gmail.com>
2026-02-09 23:55:45 +0900

    20260209_stack !리뷰&

--- classify_by_day/al_20260209.py
+++ classify_by_day/al_20260209.py
@@ -1,4 +1,12 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        return
-    
+        stack = []
+        for token in tokens:
+            if token in ["+", "-", "/", "*"]:
+                right = stack.pop()
+                left = stack.pop()
+                value = int(eval(f"{left}{token}{right}"))
+                stack.append(value)
+            else:
+                stack.append(token)
+        return int(stack[-1])
