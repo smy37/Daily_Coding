da9ee7be7afac7d395e3bb3a1e83d3f1133c999d
smy37 <seongminyang37@gmail.com>
2026-03-20 08:50:57 +0900

    20260319_two pointer&stack !리뷰&

--- classify_by_day/al_20260319.py
+++ classify_by_day/al_20260319.py
@@ -3,20 +3,34 @@ from typing import List
 class Solution:
     def trap(self, height: List[int]) -> int:
         answer = 0
-
         left = 0
+        temp_list = []
 
-        for i, h in enumerate(height[1:]):
-            if height[left] <= h:
-                if height[left] > 0:
-                    for idx in range(left+1, i+1):
-                        answer += height[left] - height[idx]
-                left = i
-            
+        for right, h in enumerate(height):
+            if height[left] < h:
+                cri = height[left]
+                ori_right = right
 
+                while temp_list and cri > temp_list[-1][1]:
+                    last_idx, last_h = temp_list.pop()
+                    answer += (right - last_idx) * (cri - last_h)
+                    right = last_idx
+                left = ori_right
+                temp_list = [[ori_right, h]]
+            else:
+                cri = h
+                last_idx = right
+                while temp_list and cri >= temp_list[-1][1]:
+                    last_idx, last_h = temp_list.pop()
+                    answer += (right-last_idx)*(cri-last_h)
+                    right = last_idx
+
+                temp_list.append([last_idx, cri])
+
+        return answer
 
 
 if __name__ == "__main__":
-    height = [0,1,0,2,1,0,1,3,2,1,2,1]
+    height = [4,2,0,3,2,5]
     sol = Solution()
-    sol.trap()
+    print(sol.trap(height))
