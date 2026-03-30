6dd83a73b9658f30b16ba31ff0dff8286200fce4
smy37 <seongminyang37@gmail.com>
2026-03-31 06:04:22 +0900

    20260330_BFS&Path !리뷰&

--- classify_by_day/al_20260330.py
+++ classify_by_day/al_20260330.py
@@ -2,30 +2,41 @@ import sys
 from collections import deque
 
 N, K = map(int, sys.stdin.readline().split())
-cri = 100000
 flag = False
 answer = abs(N-K)
-cnt = 0
 
 dq = deque()
 dq.append([N, 0])
-
+visit = {N: 0}
+cnt = {N: 1}
+if N == K:
+    print(0)
+    print(1)
+    sys.exit()
 while dq:
     cur_location, t = dq.popleft()
-    if cur_location == K and answer >= t:
-        flag = True
-        answer = t
-        cnt += 1
 
-    if t > answer:
-        break
-    if not flag:
-        if cur_location*2 <= cri:
-            dq.append([cur_location*2, t+1])
-        if cur_location +1 <= cri:
-            dq.append([cur_location +1, t + 1])
-        if cur_location -1 <= cri:
-            dq.append([cur_location - 1, t + 1])
+    for i in range(3):
+        if i == 0:
+            next_location = cur_location*2
+        elif i == 1:
+            next_location = cur_location+1
+        else:
+            next_location = cur_location-1
+
+        if next_location == K and t+1 <= answer:
+            flag = True
+            answer = t+1
+
+        if next_location not in visit:
+            visit[next_location] = t+1
+            cnt[next_location] = 0
+
+            if not flag and (next_location >=0 and next_location <= 100000):
+                dq.append([next_location, t+1])
+
+        if t+1 == visit[next_location]:
+            cnt[next_location] += cnt[cur_location]
 
 print(answer)
-print(cnt)
+print(cnt[K])
