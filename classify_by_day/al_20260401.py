6cff091224bd95944bdbe9e09d337c9fb60f30eb
smy37 <seongminyang37@gmail.com>
2026-04-02 00:02:06 +0900

    20260402_string !리뷰&

--- classify_by_day/al_20260401.py
+++ classify_by_day/al_20260401.py
@@ -5,37 +5,71 @@ cri = ["100+1+", "01"]
 T = int(sys.stdin.readline())
 
 for _ in range(T):
-    string = sys.stdin.readline().strip()
-    cur = 0
+    # string = sys.stdin.readline().strip()
+    # cur = 0
+    # flag = True
+    # while cur <= len(string)-1:
+    #     if string[cur:cur+2] == "10":
+    #         cur += 2
+    #         if cur > len(string)-1 or string[cur] != "0":
+    #             flag = False
+    #             break
+    #
+    #         for i in range(cur, len(string)):
+    #             if string[i] == "1":
+    #                 cur = i
+    #                 break
+    #
+    #         if cur > len(string)-1 or string[cur] != "1":
+    #             flag = False
+    #             break
+    #
+    #         for i in range(cur, len(string)):
+    #             if string[i] == "0":
+    #                 cur = i
+    #                 break
+    #         if cur == len(string)-1:
+    #             break
+    #     elif string[cur:cur+2] == "01":
+    #         cur += 2
+    #     else:
+    #         if string[cur-1:cur+1] == "10":
+    #             cur = cur-1
+    #             continue
+    #         flag = False
+    #         break
+    # if flag:
+    #     print("YES")
+    # else:
+    #     print("NO")
+
+    s = sys.stdin.readline().strip()
+    idx = 0
     flag = True
-    while cur <= len(string)-1:
-        if string[cur:cur+2] == "10":
-            cur += 2
-            if cur > len(string)-1 or string[cur] != "0":
+    while idx < len(s):
+        if s[idx:idx+3] == "100":
+            for i in range(idx+3, len(s)):
+                if s[i] == "1":
+                    idx = i
+                    break
+            else:
                 flag = False
                 break
-
-            for i in range(cur, len(string)):
-                if string[i] == "1":
-                    cur = i
+            for i in range(idx+1, len(s)):
+                if s[i] == "0":
                     break
-
-            if cur > len(string)-1 or string[cur] != "1":
+            else:
+                break
+            if i > idx+1 and s[i-1:i+2] == "100":
+                idx = i-1
+            elif s[i:i+2] == "01":
+                idx = i
+            else:
                 flag = False
                 break
-
-            for i in range(cur, len(string)):
-                if string[i] == "0":
-                    cur = i
-                    break
-            if cur == len(string)-1:
-                break
-        elif string[cur:cur+2] == "01":
-            cur += 2
+        elif s[idx:idx+2] == "01":
+            idx = idx + 2
         else:
-            if string[cur-1:cur+1] == "10":
-                cur = cur-1
-                continue
             flag = False
             break
     if flag:
